dbb880ad3eaa81b8565f360fe76bb76927b21af8
Juandi Romero <dsavilav@unal.edu.co>
2020-04-30 19:57:04 -0500

    segundo commit

--- app/src/main/java/com/example/hello_word/MainActivity.java
+++ app/src/main/java/com/example/hello_word/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.hello_word;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import java.text.DecimalFormat;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     EditText nVO;
     EditText nVSA;
     EditText nDMO;
+    TextView nVTO;
+    TextView nVMN;
+
+    DecimalFormat fmt = new DecimalFormat("$ ###,###,###.00");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,41 +25,56 @@ public class MainActivity extends AppCompatActivity {
         nVO = findViewById(R.id.vo);
         nVSA = findViewById(R.id.vsa);
         nDMO = findViewById(R.id.dmo);
+        nVTO = findViewById(R.id.tVTO);
+        nVMN = findViewById(R.id.tVMN);
     }
-    public void boton1(View view){
-        String cVO = VO.getText().toString();
-        int VO=Integer.parseInt(cVO);
-        String cVSA = VSA.getText().toString();
-        int VSA=Integer.parseInt(cVSA);
-        String cDMO = DMO.getText().toString();
-        int DMO=Integer.parseInt(cDMO);
 
-        if((VO=0) & (VSA = 0) & (DMO = 0)){
-            Toast.makeText(this,"LLene los campos");
+    public void Button(View view) {
+
+        String cVO = nVO.getText().toString();
+        String cVSA = nVSA.getText().toString();
+        String cDMO = nDMO.getText().toString();
+
+        double VTAD=0, VMN=0;
+        if ((cVO.isEmpty() || cVSA.isEmpty()) || cDMO.isEmpty()) {
+            Toast toast=Toast.makeText(this,"Llene todos los campos",Toast.LENGTH_SHORT);
+            toast.show();
         }
-        else{
+        else {
+            int VO = Integer.parseInt(cVO);
+            int VSA = Integer.parseInt(cVSA);
+            int DMO = Integer.parseInt(cDMO);
             VTAD = VO * 1.2142;
             //VTAD = Format(VTAD, "$#,##")
-            if (DMO <=210) {
+            if (DMO <= 210) {
                 VMN = VSA * 1.2142;
                 //777
-            }
-            else if( DMO >= 211 & DMO <= 360) {
+            } else if (DMO >= 211 && DMO <= 360) {
                 VMN = VSA * 0.9 * 1.2142;
-            }
-            else if( DMO >= 361 & DMO <= 540) {
+            } else if (DMO >= 361 && DMO <= 540) {
                 VMN = VSA * 0.8 * 1.2142;
-            }
-            else if(DMO >= 541 & DMO <= 720){
+            } else if (DMO >= 541 && DMO <= 720) {
                 VMN = VSA * 0.7 * 1.2142;
-            }
-            else if( DMO >= 721){
-                VMN=VSA*0.6*1.2142;
+            } else if (DMO >= 721) {
+                VMN = VSA * 0.6 * 1.2142;
             }
 
-            if((VSA - VMN) >= 5000000){
-                VMN = VSA - 5000000
+            if ((VSA - VMN) >= 5000000) {
+                VMN = VSA - 5000000;
             }
+            Toast toast2=Toast.makeText(this,"Nuevo resultado",Toast.LENGTH_LONG);
+            toast2.show();
+            String x=(fmt.format(VTAD));
+            nVTO.setText(x);
+            nVMN.setText(fmt.format(VMN));
+
+        }
     }
-
+    public void Limpiar (View view){
+        nVO.setText("");
+        nVSA.setText("");
+        nDMO.setText("");
+        nVTO.setText("0");
+        nVMN.setText("0");
+    }
 }
